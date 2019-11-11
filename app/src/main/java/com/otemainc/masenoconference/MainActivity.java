fb563cc8f7b48e26a1a9153a037cfa3be907e523
TheOnlySmartBoy <o2jose43@gmail.com>
2019-11-11 15:01:03 +0300

    Initial commit

---
+++ app/src/main/java/com/otemainc/masenoconference/MainActivity.java
@@ -0,0 +1,24 @@
+package com.otemainc.masenoconference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+}
