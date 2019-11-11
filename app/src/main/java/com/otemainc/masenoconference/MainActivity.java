d8cdc684f8c891c0fd56a383093eb96d33c82e73
TheOnlySmartBoy <o2jose43@gmail.com>
2019-11-11 17:29:52 +0300

    Added Web view

--- app/src/main/java/com/otemainc/masenoconference/MainActivity.java
+++ app/src/main/java/com/otemainc/masenoconference/MainActivity.java
@@ -1,24 +1,83 @@
 package com.otemainc.masenoconference;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webcontainer;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webcontainer = findViewById(R.id.container);
+        progressBar = findViewById(R.id.progressBar);
+        if(isNetworkAvailable()){
+            WebSettings Settings = webcontainer.getSettings();
+            Settings.setJavaScriptEnabled(true);
+            webcontainer.loadUrl("http://conference.maseno.ac.ke/");
+            webcontainer.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                    super.onPageStarted(view, url, favicon);
+                    progressBar.setVisibility(View.VISIBLE);
+                    setTitle("Loading...");
+                }
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+                    progressBar.setVisibility(View.GONE);
+                    setTitle(view.getTitle());
+                }
+            });
+        }else{
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage("No internet connectivity found please check that wifi or cellular network is turned on and that you have active internet connection then try again");
+            alertDialogBuilder.setPositiveButton("Ok",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface arg0, int arg1) {
+                            progressBar.setVisibility(View.GONE);
+                            startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+                        }
+                    });
+            alertDialogBuilder.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
 
     }
+    public void onBackPressed(){
+        if(webcontainer.canGoBack()){
+            webcontainer.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
     private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager
-                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
+
 }
